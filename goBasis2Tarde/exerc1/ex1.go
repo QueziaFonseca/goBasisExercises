a3d7e020e5b5aa5fc30164dc791188c3e485f6b4
QueziaFonseca <qfonseca1@gmail.com>
2023-05-29 23:15:04 -0300

    corrige exer1-Tarde-goBasis2

--- goBasis2Tarde/exerc1/ex1.go
+++ goBasis2Tarde/exerc1/ex1.go
@@ -1,20 +1,33 @@
 package main
 
 import "fmt"
+/* struct contendo
+Nome: [Nome do aluno]
+Sobrenome: [Sobrenome do aluno]
+RG: [RG do aluno]
+Data de admissão: [Data de admissão do aluno]*/
 
-// Nome: [Nome do aluno]
-// Sobrenome: [Sobrenome do aluno]
-// RG: [RG do aluno]
-// Data de admissão: [Data de admissão do aluno]
-var nome string
-var sobrenome string
-var RG string
-var dataDeAdmissão string
+type aluno struct {
+	Nome string
+	Sobrenome string
+	RG string
+	DataDeAdmissão string
+}
 
-func cadastro (nome, sobrenome, RG, dataDeAdmissão string)   (string string string string) {
-	return nome, sobrenome, RG, dataDeAdmissão
+
+func (a aluno) detalhe() {
+	fmt.Println("Nome do aluno:", a.Nome)
+	fmt.Println("Sobrenome do aluno:", a.Sobrenome)
+	fmt.Println("RG do aluno:", a.RG)
+	fmt.Println("Data de Admissão do aluno:", a.DataDeAdmissão)
 }
 
 func main() {
-	fmt.Println(cadastro(nome, sobrenome, RG, dataDeAdmissão))
+	aluno1 := aluno{
+		Nome: "Diego",
+		Sobrenome: "Limas",
+		RG: "123456",
+		DataDeAdmissão: "01/01/2023",
+	}
+	aluno1.detalhe()
 }
